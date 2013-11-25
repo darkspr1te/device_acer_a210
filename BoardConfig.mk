149c6930465e89d874a7cf982345e37fa5c46a1f
darkspr1te <darkspr1te@darkspr1te.(none)>
2013-11-25 17:57:18 +0200

    Clean up, more t30-common bring ups, external sd fstab changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,11 @@
 # inherit from the proprietary version
 -include vendor/acer/a210/BoardConfigVendor.mk
+include device/acer/t30-common/BoardConfigCommon.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := picasso_e2
-TARGET_OTA_ASSERT_DEVICE := picasso_e2, a210_pa_cus1
-TARGET_KERNEL_CONFIG := tegra_a210_defconfig
+TARGET_OTA_ASSERT_DEVICE := picasso_e2,a210_pa_cus1
+TARGET_KERNEL_CONFIG := picasso_e2a_defconfig
+TARGET_KERNEL_SOURCE := kernel/acer/kernel
 BOARD_BLUEDROID_VENDOR_CONF := device/acer/a210/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a210/bluetooth
 
@@ -12,6 +14,5 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 6291456
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 629145600
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 29905387520
 BOARD_FLASH_BLOCK_SIZE := 4096
-#should now include t30 common for common pre set values, over whats needed here
-include device/acer/t30-common/BoardConfigCommon.mk
+
 
