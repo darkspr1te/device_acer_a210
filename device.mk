0c8c1f74dac74e16d3ffbcfaeb4cca58bbe56c84
darkspr1te <darkspr1te@darkspr1te.(none)>
2013-11-26 17:20:54 +0200

    call inherit device_base problem

--- device.mk
+++ device.mk
@@ -13,8 +13,8 @@ PRODUCT_BOOTLOADER := picasso_e2
 
 # Inherit t30-common
 
-
-
+$(call inherit-product, device/acer/t30-common/device_base.mk)
+#include device/acer/t30-common/device_base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 
@@ -30,10 +30,10 @@ DEVICE_PACKAGE_OVERLAYS += device/acer/a210/overlay
 
 
 PRODUCT_COPY_FILES += \
-    device/acer/a210/prebuilt/ramdisk/init.picasso_e2.rc:root/init.picasso_e2.rc \
-    device/acer/a210/prebuilt/ramdisk/fstab.picasso_e2:root/fstab.picasso_e2 \
-    device/acer/a210/prebuilt/ramdisk/ueventd.picasso_e2.rc:root/ueventd.picasso_e2.rc \
+  #  device/acer/a210/prebuilt/ramdisk/init.picasso_e2.rc:root/init.picasso_e2.rc \
+  #  device/acer/a210/prebuilt/ramdisk/fstab.picasso_e2:root/fstab.picasso_e2 \
+  #  device/acer/a210/prebuilt/ramdisk/ueventd.picasso_e2.rc:root/ueventd.picasso_e2.rc \
  #   device/acer/a210/prebuilt/ramdisk/init.picasso_e2.usb.rc:root/init.picasso_e2.usb.rc \
 
 
-$(call inherit-product, device/acer/t30-common/device_base.mk)
+#$(call inherit-product, device/acer/t30-common/device_base.mk)
